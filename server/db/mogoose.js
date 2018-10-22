f2050a207859349b55265b28c8760dbc508a2380
Sajid S Ahamad <sajid.a@BRILLIO.COM>
2018-10-22 18:23:44 +0530

    deploying to heroku

--- server/db/mogoose.js
+++ server/db/mogoose.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp',(err,client)=>{
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds137913.mlab.com:37913/todoappdb'||'mongodb://localhost:27017/TodoApp',(err,client)=>{
     if(err){
         return console.log(err);
     }
