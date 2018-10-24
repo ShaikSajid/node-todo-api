d201e52fe8ac4e893561b2a162444a07ded848e8
Sajid S Ahamad <sajid.a@BRILLIO.COM>
2018-10-24 12:09:37 +0530

    changes made

--- server/db/mogoose.js
+++ server/db/mogoose.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://Todos:qwerty@123@ds239703.mlab.com:39703/todos'||'mongodb://localhost:27017/TodoApp',(err,client)=>{
+mongoose.connect('mongodb://localhost:27017/TodoApp',(err,client)=>{
     if(err){
         return console.log(err);
     }
